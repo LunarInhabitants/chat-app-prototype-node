adb6a4f1b005e62f81b03eccb62de267eef8000a
sam <sam@samspain.com>
2021-08-02 23:30:00 +0100

    Send message to all clients and get all messages on connect

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,46 @@
-import express from 'express';
+import express, { request, response } from "express";
+import http from "http";
+import WebSocket from "ws";
+
 const app = express();
-const port = 8080; // default port to listen
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!!" );
-} );
+const httpServer = http.createServer(app);
 
-// start the Express server
-app.listen( port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+const webSocketServer = new WebSocket.Server({ server: httpServer });
+
+interface ChatRequest {
+  type: string;
+  content: string;
+}
+
+let chatMessages: Array<string> = [];
+
+webSocketServer.on("connection", (webSocket: WebSocket) => {
+  webSocket.onmessage = function (event) {
+    const chatRequest: ChatRequest = JSON.parse(event.data.toString());
+    if (chatRequest.type === "createMessage") {
+      chatMessages.push(chatRequest.content);
+      webSocketServer.clients.forEach((client) =>
+        client.send(chatRequest.content)
+      );
+    }
+    console.log(`received: ${event.data}`);
+    console.log(chatMessages);
+  };
+  console.log("New connection");
+  console.log(chatMessages);
+
+  webSocket.send(JSON.stringify(chatMessages));
+});
+
+httpServer.listen(8080, () => {
+  console.log(`Server started.`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Standard http endpoint");
+});
+
+app.get("/messages", (request, response) => {
+  response.send(chatMessages);
+});
